8eb5f3064ddf5c0256b58f656dc5bb37613a0ec3
Braejan Arias <ing.brayanarias@gmail.com>
2025-02-08 17:37:19 -0500

    :tada: base project created.

---
+++ src/Home.tsx
@@ -0,0 +1,13 @@
+import './Home.css'
+
+function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <h1 className="text-3xl font-bold text-center text-gray-800">
+        League of Farmers
+      </h1>
+    </div>
+  )
+}
+
+export default Home 
