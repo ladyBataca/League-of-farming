67918d97c9383c3074d1ff170f67a17d1029a9cb
Braejan Arias <ing.brayanarias@gmail.com>
2025-02-19 07:53:19 -0500

    :sparkles: add diploma

--- src/Home.tsx
+++ src/Home.tsx
@@ -148,4 +148,4 @@ function Home() {
   )
 }
 
-export default Home 
+export default Home
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Home'
+import Diploma from './pages/Diploma'
 import './styles/global.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/diploma" element={<Diploma />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
